d568198873debeadbfec730c285e191fdb45b7b3
Seanathan16 <143430735+Seanathan16@users.noreply.github.com>
2024-05-13 14:57:42 +0000

    donw

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
 // Declare your arrays here
+let Isekai = ["slime_isekai.jpg ", "shield_isekai.jpg ", "re_zero.jpg"]
+let Shonen = ["black_clover.jpg", "naruto.jpg ", "demon_slayer.jpg"]
+let School = ["jujutsu_kaisen.jpg", "windbreaker.jpg", "tokyo_revengers.jpg"];
+let results = document.querySelector(".results");
+let anime;
+
+console.log(anime)
+
+
+
+
+
+// Submit Button
+
+let submitButton = document.querySelector(".submit");
+
+submitButton.addEventListener("click", function(){
+    results.innerHTML="";
+    let S = document.querySelector(".anime");
+    S = S.value;
+
+
+if (S ==="Isekai"){
+    anime = Isekai
+  
+}
+else if (S ==="Shonen"){
+    anime = Shonen
+    
+}
+
+else if (S ==="School"){
+    anime = School
+}
+
+anime.forEach(function(g){
+    let pic = document.createElement("img");
+    pic.src = g;
+    results.appendChild(pic);  
+}
+)
+});        
+
+// $(".suggestion-button").click(function() {
+//     let e = $(".give-suggestions").val();
+//     $(".shows").text("Thanks for your suggestion! Check the console to see that it was added!"),
+//     Isekai.push(e),
+//     drama.push(e),
+//     console.log(Isekai),
+//     console.log(drama)
+// });
 
 // Make sure to declare your HTML elements as variables! 
-
-// Submit Button 
-let submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {});
